630773d2f203cbd6927aeb3269fa988d2f3d65a0
Jean-Baptiste Blanc <jbb@rocketstud.io>
2021-11-30 17:32:08 +0100

    Responsive home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import Seo from "$components/SEO.svelte";
 	import Logo from "$components/Logo.svelte";
+	import { isPhone } from "$stores/media";
 </script>
 
 <Seo />
 <div class="flex items-center justify-center h-screen w-full bg-brand-blue-light">
 	<div class="flex-row">
 		<div class="flex justify-center"><div class="w-2/5"><Logo /></div></div>
-		<div class="grid grid-cols-2 mt-20 mb-12 text-sm uppercase text-gray-500">
-			<div class="grid justify-items-end items-center pr-4">
+		<div class="grid md:grid-cols-2 mt-20 mb-12 text-sm uppercase text-gray-500">
+			<div
+				class="grid items-center justify-items-center pb-6 md:justify-items-end md:pr-4">
 				<div>développement</div>
 				<div>web + api + blockchain</div>
 			</div>
-			<div class="grid items-center pl-4">formations</div>
+			<div
+				class="grid items-center justify-items-center md:justify-items-start md:pl-4">
+				formations
+			</div>
 		</div>
 		<div class="flex justify-center">
 			<a
